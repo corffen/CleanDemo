8a4ccb7f32a81307559ff6cff5bb5072ca97beec
gordon <gordon914feng@gmail.com>
2024-06-16 22:27:55 +0800

    优化 格式化代码文件

--- app/src/main/java/com/yoyo/demo/ui/activity/clean/entity/FileInfo.kt
+++ app/src/main/java/com/yoyo/demo/ui/activity/clean/entity/FileInfo.kt
@@ -1,8 +1,12 @@
 package com.yoyo.demo.ui.activity.clean.entity
 
-data class FileInfo(val name: String, val path: String,
-                    val size: Long, val type: Int,
-                    val typeName: String,
-                    val icon: Int,
-                    val modifyTime: String,
-                    val md5: String)
+data class FileInfo(
+    val name: String,
+    val path: String,
+    val size: Long,
+    val type: Int,
+    val typeName: String,
+    val icon: Int,
+    val modifyTime: String,
+    val md5: String
+)
